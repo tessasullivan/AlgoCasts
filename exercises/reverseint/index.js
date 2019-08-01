44224a3b047318fd25c20963a18410bbadd177f0
Tessa Sullivan <tessa.sullivan@gmail.com>
2019-08-01 11:52:17 -0700

    Added 2 solutions to reverseint problem

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,20 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+
+    const reversed = n.toString().split('').reverse().join('');
+    return parseInt(reversed) * Math.sign(n);
+
+  // Solution 1
+  // Make n a string, split into char array, save off sign if there, reverse the string, add the sign, convert to integer
+  // let sign = '';
+  // let charArray = n.toString().split('');
+  // if (charArray[0] === '-') {
+  //   sign = charArray.shift();
+  // }
+  // return parseInt(sign + charArray.reverse().join(''));
+
+}
 
 module.exports = reverseInt;
